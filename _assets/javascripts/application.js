9e783afeee8dafffbd3deacbf88a160164ce73cb
gntics <alexandre.quard@gmail.com>
2014-09-09 17:56:18 +0200

    deploy new version

--- _assets/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//= require 'vendor/jquery-1.10.2.min'
-//= require 'vendor/headroom/headroom.min'
-//= require 'vendor/headroom/jQuery.headroom'
-//= require 'vendor/visible/jquery.visible'
-//= require 'vendor/typed/typed'
-//= require main
-//= require resume
