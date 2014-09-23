62bd86449218345505d1047667c1f46508c6b2ff
gntics <alexandre.quard@gmail.com>
2014-09-24 01:07:42 +0200

    add new console window style

--- _assets/javascripts/resume.js
+++ _assets/javascripts/resume.js
@@ -1,13 +1,5 @@
 $(function() {
 
-  $(".console-typing").typed({
-      strings: [
-        "Welcome ^500 :)",
-        "Welcome ^500 to my cli resume. See 'help' for more information about available commands."
-      ],
-      typeSpeed: 30
-  });
-
   var el = document.getElementById('cmd');
   if (el) {
     el.focus();
@@ -30,32 +22,45 @@ $(function() {
   function process (cmd) {
     console.log(cmd);
     if (cmd === '') {
-      $('#text-wrapper').append("<p>
-        <i class='fa fa-arrow-right arrow'></i>
-        <span class='elt name'>gntics</span>
-        <span class='elt git'>git : ( <span class='branch'>resume</span> )</span>
-        </p>");
+      $('#text-wrapper').append("
+        <p class='line'>
+          <span class='pre'>></span>
+          <span class='folder'>gntics</span>
+          <span class='git'>git:(</span>
+          <span class='branch'>resume</span>
+          <span class='git'>)</span>
+        </p>
+      ");
     }
     else{
-      $('#text-wrapper').append("<p>
-        <i class='fa fa-arrow-right arrow'></i>
-        <span class='elt name'>gntics</span>
-        <span class='elt git'>git : ( <span class='branch'>resume</span> )</span>"
-        + cmd + "</p>");
+      $('#text-wrapper').append("
+        <p class='line'>
+          <span class='pre'>></span>
+          <span class='folder'>gntics</span>
+          <span class='git'>git:(</span>
+          <span class='branch'>resume</span>
+          <span class='git'>)</span>
+          <span class='cmd'>"
+          + cmd
+          + "</span>
+        </p>
+      ");
 
       if (cmd === 'help') {
-        $('#text-wrapper').append("<p id='help'>
-          The most commonly used git commands are:<br/>
-          <span class='cmd-use'>add</span><span class='cmd-desc'>Add file contents to the index</span><br/>
-          <span class='cmd-use'>bisect</span><span class='cmd-desc'>Find by binary search the change that introduced a bug</span><br/>
-          <span class='cmd-use'>branch</span><span class='cmd-desc'>List, create, or delete branches</span><br/>
-          <span class='cmd-use'>checkout</span><span class='cmd-desc'>Checkout a branch or paths to the working tree</span><br/>
-          <span class='cmd-use'>clone</span><span class='cmd-desc'>Clone a repository into a new directory</span><br/>
-          <span class='cmd-use'>commit</span><span class='cmd-desc'>Record changes to the repository</span><br/>
-          </p>");
+        $('#text-wrapper').append("
+          <p id='help' class='line'>
+            The most commonly used 'gntics' commands are:<br/>
+            <span class='cmd-use'>education</span><span class='cmd-desc'>Add file contents to the index</span><br/>
+            <span class='cmd-use'>skills</span><span class='cmd-desc'>Add file contents to the index</span><br/>
+            <span class='cmd-use'>projects</span><span class='cmd-desc'>Add file contents to the index</span><br/>
+            <span class='cmd-use'>hobbies</span><span class='cmd-desc'>Find by binary search the change that introduced a bug</span><br/>
+            <span class='cmd-use'>learning</span><span class='cmd-desc'>List, create, or delete branches</span><br/>
+            <span class='cmd-use'>contact</span><span class='cmd-desc'>Checkout a branch or paths to the working tree</span><br/>
+          </p>
+        ");
       }
       else{
-        $('#text-wrapper').append("<p>zsh: command not found: " + cmd + " </p>");
+        $('#text-wrapper').append("<p class='line'>zsh: command not found: " + cmd + " </p>");
       }
     };
     el.value = "";
